a47f1974293cbc6ad810eb362e5a6942022cbb06
Vincent van den Braken <vincent@vandenbraken.com>
2020-10-02 21:38:21 +0200

    build: lfs and linting fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,21 @@
 module.exports = {
   root: true,
+
   parserOptions: {
     parser: '@typescript-eslint/parser',
     project: './tsconfig.json',
-    extraFileExtensions: ['.vue'],
+    extraFileExtensions: [
+      '.vue'
+    ],
+    ecmaVersion: 2020
   },
+
   env: {
     browser: true,
     jest: true,
     node: true,
   },
+
   extends: [
     'airbnb-typescript/base',
     'plugin:vue-a11y/recommended',
@@ -19,19 +25,32 @@ module.exports = {
     'prettier/vue',
     'prettier/@typescript-eslint',
   ],
+
   plugins: ['@typescript-eslint', 'vue-a11y', 'prettier', 'vue'],
+
   // add your custom rules here
   rules: {
-    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'es5', printWidth: 100 }],
+    // disable the rule for all files
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 100
+      }
+    ],
+
+        
     'import/extensions': [
       'error',
       'always',
       {
         js: 'never',
-        ts: 'never',
-      },
+        ts: 'never'
+      }
     ],
-    'no-console': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-var-requires': 0,
     'import/no-dynamic-require': 0,
@@ -39,8 +58,24 @@ module.exports = {
     'no-underscore-dangle': 0,
     'class-methods-use-this': 0,
     'vue/max-attributes-per-line': 'off',
-    'vue/component-name-in-template-casing': [1, 'kebab-case'],
+    'vue/component-name-in-template-casing': [
+      1,
+      'kebab-case'
+    ],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
+  // WARN
+  // D:\Dev\vamonos.nu\app\utils.ts
+  // 5:34  error  Object pattern argument should be typed  @typescript-eslint/explicit-module-boundary-types
+  // "overrides": [
+  //   {
+  //     // enable the rule specifically for TypeScript files
+  //     "files": ["*.ts", "*.tsx"],
+  //     "rules": {
+  //       "@typescript-eslint/explicit-module-boundary-types": ["error"]
+  //     }
+  //   }
+  // ],
   settings: {
     'import/core-modules': [
       '@nuxt/config',
@@ -67,4 +102,17 @@ module.exports = {
       },
     },
   },
+
+  'extends': [
+    'airbnb-typescript/base',
+    'plugin:vue-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/strongly-recommended',
+    'prettier',
+    'prettier/vue',
+    'prettier/@typescript-eslint',
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/prettier'
+  ]
 };
